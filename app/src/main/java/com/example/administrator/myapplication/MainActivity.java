2e8d91bbd449ec717459acb565525998db50cefb
248447581@qq.com <qq248447581>
2019-03-11 22:19:49 +0800

    test

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import me.jessyan.autosize.internal.CustomAdapt;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ////test
+    }
+
+    public void onTest(View view) {
+
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                okHttpClient.newCall(request).enqueue(new Callback() {
+//                    @Override
+//                    public void onFailure(Call call, IOException e) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onResponse(Call call, Response response) throws IOException {
+//
+//                    }
+//                });
+//            }
+//        }).start();
+
+    }
+
+}
